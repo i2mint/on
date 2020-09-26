67ebc13b50e821ed94bf751dfed524d65c76a8dd
twhalen <thor@otosense.com>
2020-09-25 18:00:03 -0700

    better init

--- on/__init__.py
+++ on/__init__.py
@@ -16,31 +16,8 @@ from on.base import (
     kv_walk,
 )
 
-from on.stores.local_store import (
-    LocalStore,
-    LocalBinaryStore,
-    LocalTextStore,
-    LocalPickleStore,
-    LocalJsonStore,
-)
-from on.stores.local_store import (
-    QuickStore,
-    QuickBinaryStore,
-    QuickTextStore,
-    QuickJsonStore,
-    QuickPickleStore,
-)
-from on.stores.local_store import DirReader, DirStore
-
-from on.misc import (
-    MiscGetter,
-    MiscGetterAndSetter,
-    misc_objs,
-    misc_objs_get,
-    get_obj,
-    set_obj,
-)
 from on.base import Store
+
 from on.trans import (
     wrap_kvs,
     disable_delitem,
@@ -53,53 +30,6 @@ from on.trans import (
     add_path_get,
     insert_aliases,
 )
-from on.trans import cache_iter  # being deprecated
-from on.access import (
-    user_configs_dict,
-    user_configs,
-    user_defaults_dict,
-    user_defaults,
-)
-from on.caching import (
-    mk_cached_store,
-    store_cached,
-    store_cached_with_single_key,
-    ensure_clear_to_kv_store,
-    flush_on_exit,
-    mk_write_cached_store,
-)
-
-from on.appendable import (
-    appendable
-)
-
-from on.stores.local_store import (
-    PickleStore,
-)  # consider deprecating and use LocalPickleStore instead?
-from on.slib.s_zipfile import (
-    ZipReader,
-    ZipFilesReader,
-    FilesOfZip,
-    FlatZipFilesReader,
-)
-
-with ModuleNotFoundIgnore():
-    from on.access import myconfigs
-with ModuleNotFoundIgnore():
-    from on.access import mystores
-
-with ModuleNotFoundIgnore():
-    from on.stores.s3_store import (
-        S3BinaryStore,
-        S3TextStore,
-        S3PickleStore,
-    )
-with ModuleNotFoundIgnore():
-    from on.stores.mongo_store import (
-        MongoStore,
-        MongoTupleKeyStore,
-        MongoAnyKeyStore,
-    )
 
 
 def kvhead(store, n=1):
